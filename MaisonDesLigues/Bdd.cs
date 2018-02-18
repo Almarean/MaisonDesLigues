a7ff41bf6e45ee4295228c4cf0ef6a2677f7d921
leoespeu <btssioleo.espeu@gmail.com>
2018-02-18 18:44:50 +0100

    -Changement du package oracle (l'ancien package déclenchait des exceptions)

--- MaisonDesLigues/Bdd.cs
+++ MaisonDesLigues/Bdd.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data;
-using Oracle.DataAccess.Client;
+using Oracle.ManagedDataAccess.Client;
 using System.Configuration;
 using System.Windows.Forms;
 using System.Collections.ObjectModel;
